ade75d0fd5d8238048896bc2ba19f363eaae3d65
NgocHieu010503 <154217329+NgocHieu010503@users.noreply.github.com>
2025-05-24 22:25:14 +0700

    Update index.php

--- index.php
+++ index.php
@@ -5,6 +5,8 @@ include 'db.php';
 <!DOCTYPE html>
 <html>
 <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Quiz Online</title>
     <link rel="stylesheet" href="style.css">
 </head>
