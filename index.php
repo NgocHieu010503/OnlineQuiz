f106447fed6596bbe294709e015b919ec6cc12f7
NgocHieu010503 <154217329+NgocHieu010503@users.noreply.github.com>
2025-05-27 11:27:33 +0700

    Update index.php

--- index.php
+++ index.php
@@ -14,20 +14,26 @@ include 'db.php';
     <form action="result.php" method="POST">
         <h2>Quiz Online</h2>
         <?php
-        $result = $conn->query("SELECT * FROM questions");
-        $i = 1;
-        while ($row = $result->fetch_assoc()):
+        $ketqua = $conn->query("SELECT * FROM questions");
+        if ($ketqua->num_rows > 0):
+            $i = 1;
+            while ($dong = $ketqua->fetch_assoc()):
         ?>
         <div>
-            <p><?php echo $i . ". " . $row['question_text']; ?></p>
+            <p><?php echo $i . ". " . $dong['question_text']; ?></p>
             <?php foreach (['a', 'b', 'c', 'd'] as $opt):
-                if (!empty($row["option_$opt"])): ?>
-                    <input type="radio" name="q<?php echo $row['id']; ?>" value="<?php echo $opt; ?>">
-                    <?php echo $row["option_$opt"]; ?><br>
+                if (!empty($dong["option_$opt"])): ?>
+                    <label>
+                        <input type="radio" name="q<?php echo $dong['id']; ?>" value="<?php echo $opt; ?>">
+                        <?php echo $dong["option_$opt"]; ?>
+                    </label><br>
             <?php endif; endforeach; ?>
         </div>
-        <?php $i++; endwhile; ?>
-        <input type="submit" value="Submit quiz">
+        <?php $i++; endwhile; else: ?>
+            <p>Không có câu hỏi nào trong cơ sở dữ liệu.</p>
+        <?php endif; ?>
+        
+        <input type="submit" value="Nộp bài">
     </form>
 </body>
 </html>
